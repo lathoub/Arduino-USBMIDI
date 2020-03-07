177c8864a1fb7a4834395e520e3e59379119a04a
lathoub <lathoub@gmail.com>
2020-03-07 17:50:47 +0100

    using lookup table for x2cin and cin2x (also added logging)

--- src/USB-MIDI.h
+++ src/USB-MIDI.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include "utility/Logging.h"
+
 #include <MIDI.h>
 #include <MIDIUSB.h>
 
@@ -9,38 +11,21 @@
 
 BEGIN_USBMIDI_NAMESPACE
 
+using namespace MIDI_NAMESPACE;
+
 class usbMidiTransport
 {
 private:
     byte mTxBuffer[4];
     size_t mTxIndex;
-    
+    midiEventPacket_t packet;
+    MidiType mStatus;
+
     Deque<byte, 44> mRxBuffer; // 44 is an arbitary number
     
-    midiEventPacket_t packet;
 
     uint8_t cableNumber;
     
-    static bool midi_is_real_time(uint8_t byte)
-    {
-        return byte == 0xf8 || (byte >= 0xfa && byte != 0xfd);
-    }
-
-    static bool midi_is_sysex_start(uint8_t byte)
-    {
-        return byte == 0xf0;
-    }
-
-    static bool midi_is_sysex_end(uint8_t byte)
-    {
-        return byte == 0xf7;
-    }
-
-    static bool midi_is_single_byte_system_common(uint8_t byte)
-    {
-        return byte >= 0xf4 && byte <= 0xf6;
-    }
-
 public:
 	usbMidiTransport(uint8_t cableNumber = 0) // cable number?
 	{
@@ -54,38 +39,57 @@ public:
         mTxIndex = 0;
     };
 
-	bool beginTransmission(MIDI_NAMESPACE::MidiType)
+	bool beginTransmission(MidiType status)
 	{
-        mTxIndex = 0;
+        // from https://www.usb.org/sites/default/files/midi10.pdf
+        // 4 USB-MIDI Event Packets
+        // Table 4-1: Code Index Number Classifications
+        static uint8_t type2cin[][2] = { {InvalidType,0}, {NoteOff,8}, {NoteOn,9}, {AfterTouchPoly,0xA}, {ControlChange,0xB}, {ProgramChange,0xC}, {AfterTouchChannel,0xD}, {PitchBend,0xE} };
+        static uint8_t system2cin[][2] = { {SystemExclusive,0}, {TimeCodeQuarterFrame,2}, {SongPosition,3}, {SongSelect,2}, {0,0}, {0,0}, {TuneRequest,5}, {SystemExclusiveEnd,0}, {Clock,0xF}, {0,0}, {Start,0xF}, {Continue,0xF}, {Stop,0xF}, {0,0}, {ActiveSensing,0xF}, {SystemReset,0xF} };
+
+        mStatus = status;
         
+        byte cin = 0;
+        if (status < SystemExclusive)
+            cin = type2cin[((status & 0xF0) >> 4) - 7][1];
+        else
+            cin = system2cin[status & 0x0F][1];
+        
+        packet.header = (cableNumber | cin);
+        packet.byte1  = 0;
+        packet.byte2  = 0;
+        packet.byte3  = 0;
+
+        mTxIndex = 0;
+
         return true;
 	};
 
 	void write(byte byte)
 	{
-        mTxBuffer[mTxIndex++] = byte;
-	};
+        if (mStatus != MidiType::SystemExclusive)
+        {
+            switch (++mTxIndex)
+            {
+                case 1:
+                    packet.byte1  = byte;
+                    break;
+                case 2:
+                    packet.byte2  = byte;
+                    break;
+                case 3:
+                    packet.byte3  = byte;
+                    break;
+            }
+        }
+        else
+        {
+            // sysex
+        }
+    };
 
 	void endTransmission()
 	{
-        midiEventPacket_t packet;
-        packet.header = 0;
-        packet.byte1  = 0;
-        packet.byte2  = 0;
-        packet.byte3  = 0;
-
-        auto status = mTxBuffer[0] & 0x0f0;
-        switch (status)
-        {
-            case MIDI_NAMESPACE::MidiType::NoteOn:
-            case MIDI_NAMESPACE::MidiType::NoteOff:
-                packet.header = cableNumber | (status >> 4);
-                packet.byte1  = mTxBuffer[0];
-                packet.byte2  = mTxBuffer[1];
-                packet.byte3  = mTxBuffer[2];
-                break;
-        }
-        
         MidiUSB.sendMIDI(packet);
         MidiUSB.flush();
 	};
@@ -99,10 +103,12 @@ public:
 
 	unsigned available()
 	{
+        // USB-MIDI -> MIDI engine
+        
         // from https://www.usb.org/sites/default/files/midi10.pdf
         // 4 USB-MIDI Event Packets
         // Table 4-1: Code Index Number Classifications
-        static byte cinToLen[16][2] = { {0,0}, {1,0}, {2,2}, {3,3}, {4,3}, {5,1}, {6,2}, {7,3}, {8,3}, {9,3}, {10,3}, {11,3}, {12,2}, {13,2}, {14,3}, {15,1} };
+        static byte cin2Len[][2] = { {0,0}, {1,0}, {2,2}, {3,3}, {4,3}, {5,1}, {6,2}, {7,3}, {8,3}, {9,3}, {10,3}, {11,3}, {12,2}, {13,2}, {14,3}, {15,1} };
 
         midiEventPacket_t packet = MidiUSB.read();
         if (packet.header != 0)
@@ -112,7 +118,7 @@ public:
  //               break;
                 
             auto cin = packet.header & 0x0f;
-            auto len = cinToLen[cin][1];
+            auto len = cin2Len[cin][1];
             switch (len)
             {
                 case 1:
